ae37810fd9cdaba86d17be7656924f9338e0fd9d
Zach <ss3808924@gmail.com>
2023-01-13 11:27:09 +0800

    Add Gin Logger and BasicAuth

--- main.go
+++ main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"SampleGoGin/database"
+	"SampleGoGin/middlewares"
 	. "SampleGoGin/src"
+	"io"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+func setupLogging() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
+	setupLogging()
 	router := gin.Default()
+
+	router.Use(gin.BasicAuth(gin.Accounts{"Tom": "123456"}), middlewares.Logger())
+
 	v1 := router.Group("/v1")
 	AddUserRouter(v1)
 
